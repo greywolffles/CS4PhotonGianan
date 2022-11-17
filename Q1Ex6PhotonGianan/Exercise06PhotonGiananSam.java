584a064201c9c00779b94d51dd10baf5951e30fa
sam <93103822+greywolffles@users.noreply.github.com>
2022-11-17 08:57:07 +0800

    Update Exercise06PhotonGiananSam.java

--- Q1Ex6PhotonGianan/Exercise06PhotonGiananSam.java
+++ Q1Ex6PhotonGianan/Exercise06PhotonGiananSam.java
@@ -15,7 +15,32 @@ public class Exercise06PhotonGiananSam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        FireType charmander = new FireType("Charmander", 75, 10);
+        GrassType bulbasaur = new GrassType("Bulbasaur", 75, 10);
+        WaterType squirtle = new WaterType("Squirtle", 75, 10);
+        
+        System.out.println("Grass vs. Fire");
+        while ((bulbasaur.getHP() > 0) && (charmander.getHP() > 0)){
+            bulbasaur.attack(charmander);
+            charmander.attack(bulbasaur);
+        }
+        bulbasaur.resetHealth();
+        charmander.resetHealth();
+        
+        System.out.println("Fire vs. Water");
+        while ((charmander.getHP() > 0) && (squirtle.getHP() > 0)){
+            charmander.attack(squirtle);
+            squirtle.attack(charmander);
+        }
+        charmander.resetHealth();
+        squirtle.resetHealth();
+        
+        System.out.println("Water vs. Grass");
+        while ((squirtle.getHP() > 0) && (bulbasaur.getHP() > 0)){
+            squirtle.attack(bulbasaur);
+            bulbasaur.attack(squirtle);
+        }
+        squirtle.resetHealth();
+        bulbasaur.resetHealth();
     }
-    
 }
